4aa4ad9aee57a5910893f940cccb67b933863f0e
a289459798 <289459798@qq.com>
2023-02-13 21:48:44 +0800

    水印功能

--- internal/logic/image/watermarklogic.go
+++ internal/logic/image/watermarklogic.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	"fmt"
 	gogpt "github.com/sashabaranov/go-gpt3"
+	"image"
+	"image/png"
+	"net/http"
+	"os"
 
 	"chatgpt-tools/internal/svc"
 	"chatgpt-tools/internal/types"
@@ -26,11 +30,40 @@ func NewWatermarkLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Waterma
 }
 
 func (l *WatermarkLogic) Watermark(req *types.WatermarkRequest) (resp *types.ImageResponse, err error) {
+
+	imgResp, err := http.Get(req.Url)
+	if err != nil {
+		return nil, err
+	}
+	defer imgResp.Body.Close()
+
+	// 解码图片
+	img, _, err := image.Decode(imgResp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	out, err := os.Create("tmp/image.png")
+	if err != nil {
+		return nil, err
+	}
+	defer out.Close()
+
+	err = png.Encode(out, img)
+	if err != nil {
+		return nil, err
+	}
+
+	//defer os.Remove("tmp/image.png")
+
 	gptReq := gogpt.ImageEditRequest{
-		Image: ,
+		Image:  out,
+		Mask:   out,
 		Prompt: fmt.Sprintf("在文字%s方加上文字大小为%d颜色为%s透明度为%f的水印，水印内容为:%s", req.Position, req.FontSize, req.Color, req.Opacity, req.Content),
 		N:      1,
+		Size:   "512x512",
 	}
+
 	ctx := context.Background()
 	stream, err := l.svcCtx.GptClient.CreateEditImage(ctx, gptReq)
 	if err != nil {
--- internal/svc/servicecontext.go
+++ internal/svc/servicecontext.go
@@ -3,16 +3,9 @@ package svc
 import (
 	"chatgpt-tools/internal/config"
 	"chatgpt-tools/internal/middleware"
-	"chatgpt-tools/model"
 	gogpt "github.com/sashabaranov/go-gpt3"
 	"github.com/zeromicro/go-zero/rest"
-	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
-	"gorm.io/gorm/schema"
-	"log"
-	"os"
-	"time"
 )
 
 type ServiceContext struct {
@@ -24,26 +17,26 @@ type ServiceContext struct {
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
-	db, _ := gorm.Open(mysql.Open(c.Mysql.DataSource), &gorm.Config{
-		NamingStrategy: schema.NamingStrategy{
-			TablePrefix:   "gpt_",
-			SingularTable: true,
-		},
-		Logger: logger.New(
-			log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer（日志输出的目标，前缀和日志包含的内容——译者注）
-			logger.Config{
-				SlowThreshold:             time.Second, // 慢 SQL 阈值
-				LogLevel:                  logger.Info, // 日志级别
-				IgnoreRecordNotFoundError: true,        // 忽略ErrRecordNotFound（记录未找到）错误
-				Colorful:                  false,       // 禁用彩色打印
-			},
-		),
-	})
-
-	db.AutoMigrate(&model.User{})
+	//db, _ := gorm.Open(mysql.Open(c.Mysql.DataSource), &gorm.Config{
+	//	NamingStrategy: schema.NamingStrategy{
+	//		TablePrefix:   "gpt_",
+	//		SingularTable: true,
+	//	},
+	//	Logger: logger.New(
+	//		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer（日志输出的目标，前缀和日志包含的内容——译者注）
+	//		logger.Config{
+	//			SlowThreshold:             time.Second, // 慢 SQL 阈值
+	//			LogLevel:                  logger.Info, // 日志级别
+	//			IgnoreRecordNotFoundError: true,        // 忽略ErrRecordNotFound（记录未找到）错误
+	//			Colorful:                  false,       // 禁用彩色打印
+	//		},
+	//	),
+	//})
+	//
+	//db.AutoMigrate(&model.User{})
 	return &ServiceContext{
-		Config:     c,
-		Db:         db,
+		Config: c,
+		//Db:         db,
 		Auth:       middleware.NewAuthMiddleware().Handle,
 		AuthAndUse: middleware.NewAuthAndUseMiddleware().Handle,
 		GptClient:  gogpt.NewClient(c.OpenAIKey),
