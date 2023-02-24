7a4c1cf6b3f97b3b00168910bec337f7138f9fb7
zhangzy <zhangzy0816@gmail.com>
2023-02-24 09:43:39 +0800

    增加违禁词提示

--- common/utils/filter.go
+++ common/utils/filter.go
@@ -1,15 +1,16 @@
 package utils
 
 import (
+	"fmt"
 	"github.com/importcjj/sensitive"
 )
 
 func Filter(str string) string {
 	filter := sensitive.New()
 	filter.LoadWordDict("data/sensitive_words_lines.txt")
-	valid, _ := filter.Validate(str)
+	valid, s := filter.Validate(str)
 	if valid {
 		return ""
 	}
-	return "人类的问题太深奥，三目无法回答此类问题"
+	return fmt.Sprintf("存在一下违禁词：%s\n人类的问题太深奥，三目无法回答此类问题。", s)
 }
