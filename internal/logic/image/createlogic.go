008bd1536aa633473b18ce906df628d372aa4c11
a289459798 <289459798@qq.com>
2023-03-11 09:05:10 +0800

    画图改成3次

--- internal/logic/image/createlogic.go
+++ internal/logic/image/createlogic.go
@@ -11,6 +11,7 @@ import (
 	"encoding/json"
 	"errors"
 	"github.com/zeromicro/go-zero/core/logx"
+	"time"
 )
 
 type CreateLogic struct {
@@ -33,13 +34,18 @@ func (l *CreateLogic) Create(req *types.ImageRequest) (resp *types.ImageResponse
 	if valid != "" {
 		return nil, errors.New(valid)
 	}
+	uid, _ := l.ctx.Value("uid").(json.Number).Int64()
+	amount := model.NewAccount(l.svcCtx.Db).GetAccount(uint32(uid), time.Now())
+	if amount.ChatAmount-amount.ChatUse < 3 {
+		return nil, errors.New("次数已用完")
+	}
+
 	prompt := "帮我生成一张图片，图片里面需要包含以下内容：" + req.Content
 	stream, err := sanmuai.NewOpenAi(l.ctx, l.svcCtx).CreateImage(prompt)
 	if err != nil {
 		return nil, err
 	}
 
-	uid, _ := l.ctx.Value("uid").(json.Number).Int64()
 	service.NewRecord(l.svcCtx.Db).Insert(&model.Record{
 		Uid:     uint32(uid),
 		Type:    "image/create",
