918063285c975fd28ce876e7b595086fde77dda6
zhangzy <zhangzy0816@gmail.com>
2023-03-17 13:39:06 +0800

    修改令牌长度

--- common/utils/sanmuai/openai.go
+++ common/utils/sanmuai/openai.go
@@ -64,7 +64,7 @@ func (ai *OpenAi) CreateChatCompletionStream(content []gogpt.ChatCompletionMessa
 	gptReq := gogpt.ChatCompletionRequest{
 		Model:            gogpt.GPT3Dot5Turbo,
 		Messages:         content,
-		MaxTokens:        2536,
+		MaxTokens:        3036,
 		Temperature:      0.7,
 		TopP:             1,
 		FrequencyPenalty: 0,
