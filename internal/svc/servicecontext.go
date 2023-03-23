76e1bb8ef3d4719b86609edb21b8916ee82b5e8e
zhangzy <zhangzy0816@gmail.com>
2023-03-23 11:25:11 +0800

    修改算力计算

--- internal/svc/servicecontext.go
+++ internal/svc/servicecontext.go
@@ -3,6 +3,7 @@ package svc
 import (
 	"chatgpt-tools/internal/config"
 	"chatgpt-tools/internal/middleware"
+	"chatgpt-tools/model"
 	"github.com/zeromicro/go-zero/rest"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
