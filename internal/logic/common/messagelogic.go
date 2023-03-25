777fda784d0ae2b5b5ed78d37921898b90da3688
a289459798 <289459798@qq.com>
2023-03-25 16:38:31 +0800

    数据为空

--- internal/logic/common/messagelogic.go
+++ internal/logic/common/messagelogic.go
@@ -1,12 +1,10 @@
 package common
 
 import (
-	"chatgpt-tools/model"
-	"context"
-	"errors"
-
 	"chatgpt-tools/internal/svc"
 	"chatgpt-tools/internal/types"
+	"chatgpt-tools/model"
+	"context"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -29,7 +27,7 @@ func (l *MessageLogic) Message() (resp *types.MessageResponse, err error) {
 	message := &model.Message{}
 	l.svcCtx.Db.Where("status = 1").Find(&message)
 	if message.ID == 0 {
-		return nil, errors.New("数据为空")
+		return nil, nil
 	}
 
 	return &types.MessageResponse{
