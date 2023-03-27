0c30ce0db45380eda676dd392d23ef78219b7624
zhangzy <zhangzy0816@gmail.com>
2023-03-27 10:13:01 +0800

    修改次数校验接口

--- internal/logic/common/validchatlogic.go
+++ internal/logic/common/validchatlogic.go
@@ -5,7 +5,6 @@ import (
 	"chatgpt-tools/model"
 	"context"
 	"encoding/json"
-	"errors"
 	"math/rand"
 	"strconv"
 	"time"
@@ -33,20 +32,13 @@ func NewValidChatLogic(ctx context.Context, svcCtx *svc.ServiceContext) *ValidCh
 func (l *ValidChatLogic) ValidChat(req *types.ValidRequest) (resp *types.ValidResponse, err error) {
 	uid, _ := l.ctx.Value("uid").(json.Number).Int64()
 	amount := model.NewAccount(l.svcCtx.Db).GetAccount(uint32(uid), time.Now())
-
 	consume := utils.GetSuanLi(uint32(uid), req.Content, req.Params, l.svcCtx.Db)
-
-	if amount.ChatAmount < (amount.ChatUse + uint32(consume)) {
-		return nil, errors.New("次数已用完")
-	}
-
 	showAd := false
 
 	randomNum := rand.Intn(10) + 1
 	if randomNum > 6 {
 		showAd = true
 	}
-
 	return &types.ValidResponse{
 		Data:    strconv.Itoa(int(amount.ChatAmount) - int(amount.ChatUse)),
 		ShowAd:  showAd,
