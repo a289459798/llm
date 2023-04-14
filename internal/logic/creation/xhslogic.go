68cb270470ec7c4d07559114770471ea7a0202ac
zhangzy <zhangzy0816@gmail.com>
2023-04-14 11:09:22 +0800

    增加朋友圈和小红书工具

--- internal/logic/creation/xhslogic.go
+++ internal/logic/creation/xhslogic.go
@@ -1,7 +1,16 @@
 package creation
 
 import (
+	"chatgpt-tools/common/utils"
+	"chatgpt-tools/common/utils/sanmuai"
+	"chatgpt-tools/model"
+	"chatgpt-tools/service"
 	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	gogpt "github.com/sashabaranov/go-openai"
+	"io"
 	"net/http"
 
 	"chatgpt-tools/internal/svc"
@@ -25,7 +34,79 @@ func NewXhsLogic(ctx context.Context, svcCtx *svc.ServiceContext) *XhsLogic {
 }
 
 func (l *XhsLogic) Xhs(req *types.XhsRequest, w http.ResponseWriter) (resp *types.CreationResponse, err error) {
-	// todo: add your logic here and delete this line
+	tools := model.ToolsAD
+	uid, _ := l.ctx.Value("uid").(json.Number).Int64()
+	user := model.AIUser{Uid: uint32(uid)}.Find(l.svcCtx.Db)
+	message, isFirst, err := model.Record{Uid: uint32(uid), ChatId: req.ChatId, Type: tools}.GetMessage(l.svcCtx.Db, user)
+	if err != nil {
+		return nil, err
+	}
+
+	content := req.Content
+	showContent := ""
+	title := ""
+	if isFirst {
+		title = req.Content
+		content = fmt.Sprintf("我希望在小红书发表以下内容：%s", req.Content)
+	}
+
+	message = append(message, gogpt.ChatCompletionMessage{
+		Role:    gogpt.ChatMessageRoleUser,
+		Content: content,
+	})
+
+	// 创建上下文
+	w.Header().Set("Content-Type", "text/event-stream")
+	ctx, cancel := context.WithCancel(l.ctx)
+	defer cancel()
+	ch := make(chan struct{})
+	stream, err := sanmuai.NewOpenAi(ctx, l.svcCtx).CreateChatCompletionStream(message)
+	if err != nil {
+		return nil, err
+	}
+	defer stream.Close()
+	result := ""
+	go func() {
+		for {
+			response, err := stream.Recv()
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			if err != nil {
+				break
+			}
+			if len(response.Choices) > 0 {
+				w.Write([]byte(utils.EncodeURL(response.Choices[0].Delta.Content)))
+				result += response.Choices[0].Delta.Content
+				if f, ok := w.(http.Flusher); ok {
+					f.Flush()
+				}
+			}
+
+		}
+		close(ch)
+	}()
+
+	select {
+	case <-ch:
+		// 处理已完成
+		logx.Infof("EventStream logic finished")
+	case <-ctx.Done():
+		// 处理被取消
+		logx.Errorf("EventStream logic canceled")
+	}
+	if result == "" {
+		return nil, errors.New("数据为空")
+	}
+	service.NewRecord(l.svcCtx.Db).Insert(&model.Record{
+		Uid:         uint32(uid),
+		Type:        tools,
+		Title:       title,
+		Content:     content,
+		ShowContent: showContent,
+		ChatId:      req.ChatId,
+		Result:      result,
+	}, nil)
 
 	return
 }
