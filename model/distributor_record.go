e0cee9b74fba3458ffe081b3abcab4c86b2e1582
zhangzy <zhangzy0816@gmail.com>
2023-04-19 14:49:58 +0800

    修复推广bug

--- model/distributor_record.go
+++ model/distributor_record.go
@@ -7,7 +7,7 @@ import (
 
 type DistributorRecord struct {
 	ID             uint32    `gorm:"primary_key" json:"id"`
-	DistributorUid uint32    `json:"distributor_uid" gorm:"uniqueIndex:uk_distributor_uid"`
+	DistributorUid uint32    `json:"distributor_uid" gorm:"index:ik_distributor_uid"`
 	Uid            uint32    `json:"uid" gorm:"uniqueIndex:uk_uid"`
 	CreatedAt      time.Time `gorm:"column:created_at;type:TIMESTAMP;default:CURRENT_TIMESTAMP;<-:create" json:"created_at,omitempty"`
 	UpdateAt       time.Time `gorm:"column:update_at;type:TIMESTAMP;default:CURRENT_TIMESTAMP  on update current_timestamp" json:"update_at,omitempty"`
